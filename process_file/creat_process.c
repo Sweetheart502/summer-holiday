0497bb909267e89c0117a22777de20e67c224f89
Sweetheart502 <869780307@qq.com>
2014-07-21 11:12:30 +0800

    07/21 -- 11:12

--- process_file/creat_process.c
+++ process_file/creat_process.c
@@ -30,6 +30,16 @@ void error ( char * err_string, int line )
 	exit ( -1 );
 }
 
+	/*
+	 * fork函数创建当前进程的一份拷贝。
+	 * 当前进程成为父进程，新进程成为子进程。
+	 * 返回值：成功时，在父进程中返回子进程ID,在子进程中返回0;
+	 * 	　 失败时，返回值为-1.
+	 * pid=-1: 当前进程数已达到系统规定的上限，errno值为EAGAIN;
+	 * 	   系统可提供使用的内存不足，errno值为ENOMEM.
+	 * 创建成功后，进程的执行顺序取决于内核所使用的调度算法。
+	 */
+
 int main(int argc, char *argv[])
 {
 	pid_t pid;
