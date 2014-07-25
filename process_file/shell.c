9a97ca9d62061dbc8b3497e1a0723233def69e26
Sweetheart502 <869780307@qq.com>
2014-07-25 17:35:22 +0800

    07/25 -- 17:35

--- process_file/shell.c
+++ process_file/shell.c
@@ -25,40 +25,38 @@
 
 #define N 80
 
-//char * p;
-
 void setup ( char *buf, char * argv[] )
 {
-	int i = 0;
-	char *p;
+	int 	i = 0; 					
+	char 	*p;
 
 	for ( p = buf; ; p++ ) {
 		argv[i++] = p;
 
-		while ( *p != ' ' && *p != '\0' ) {
+		while ( *p != ' ' && *p != '\0' ) { 	/* 没有出现空格或者没有到达字符串末尾，都写入同一个字符串中，作为参数或命令 */
 			p++;
 		}
 
-		if ( *p == '\0' ) /* 字符串结束 */
+		if ( *p == '\0' ) 			/* 字符串结束 */
 		      break;
 
-		*p = '\0';
+		*p = '\0'; 				/* 使每一个字符串都以'\0'结尾 */
 	}
 
-	argv[i] = NULL; 	/* argv末尾是以NULL结束的 */
+	argv[i] = NULL; 				/* argv末尾是以NULL结束的 */
 
 	execvp ( argv[0], argv );
 }
 
 int main( )
 {
-	char buf[N];
-	char * argv[N];
+	char buf[N]; 					/* 存储输入的命令 */
+	char * argv[N]; 				/* 获得命令及参数，即将输入内容进行分割 */
 
 	while ( 1 ) {
-		printf ( "sweetheart@xiao $ " );
-		gets ( buf );
-		setup ( buf, argv );
+		printf ( "sweetheart@xiao $ " ); 	/* 输出shell的提示符 */
+		gets ( buf ); 				/* 输入命令字符串 */
+		setup ( buf, argv ); 			/* 执行命令信息 */
 	}
 
 	return EXIT_SUCCESS;
