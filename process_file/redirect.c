625a544c611fc4de1de451e9c6e25ae584d7d2ff
Sweetheart502 <869780307@qq.com>
2014-07-27 17:47:44 +0800

    07/27 -- 17:47

---
+++ process_file/redirect.c
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  redirect.c
+ *
+ *    Description:  了解重定向。
+ *
+ *        Version:  1.0
+ *        Created:  2014年07月27日 17时40分11秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sweetheart502, liyingxiao502@gmail.com
+ *        Company:  Class 1304 of Software Engineering
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	system ( "ls > file1" );
+	/* 
+	 * 表示将ls命令执行后结果送到指定文件file1中，
+	 * 此时的文件存在则清除文件原内容，不存在则新建一个该文件。
+	 */
+
+	 system ( "ls >> file2" );
+	 /*
+	  * 将命令执行的显示结果附加到指定文件中。
+	  * 此时文件存在则直接追加写入文件，不存在则新建一个该文件。
+	  */
+
+	system ( "ls >! file3" );
+	/*
+	 * 将命令执行显示的结果送至指定的文件中。
+	 * 此时文件存在则覆盖，不存在则不能进行。
+	 */
+
+	return EXIT_SUCCESS;
+}
+
