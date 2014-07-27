1c85bda42a3a249bb5da723626efea5f04878198
Sweetheart502 <869780307@qq.com>
2014-07-27 23:45:53 +0800

    07/27 -- 23:45

--- process_file/redirect.c
+++ process_file/redirect.c
@@ -16,11 +16,8 @@
  * =====================================================================================
  */
 
-#include <unistd.h>
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main(int argc, char *argv[])
 {
