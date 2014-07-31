a2ae5ac5d19d07e13dd8a89cba496a30ebfcbef4
Sweetheart502 <869780307@qq.com>
2014-07-31 20:41:34 +0800

    07/31 -- 20:41

--- thread_file/error_show.c
+++ thread_file/error_show.c
@@ -26,22 +26,26 @@
 FILE * open_file ( char * filename )
 {
 	FILE * fp;
-	errno = 0;
+	//errno = 0;
 
 	fp = fopen ( filename, "rt" );
 
 	if ( fp == NULL ) {
 		//perror ( "fopen" );
 		/* perror打印错误信息到stderr即屏幕或命令行终端 */
+		
+		printf ( "errno is %d.\n", errno ); //失败时的errno
 
 		printf ( "error:%s -> %s\n", filename, strerror ( errno ) );
 		/* strerror函数根据参数errnum提供的错误码获取一个描述错误信息的字符串 */
 		exit ( 1 );
 	} else {
+		printf ( "errno is %d.\n", errno ); //成功时的errno
 		return fp;
 	}
 }
 
+/* 主函数 */
 int main(int argc, char *argv[])
 {
 	char * filename = "test";
