da8d66dd96c047f97f5415a14f6444a966b3b03b
Sweetheart502 <869780307@qq.com>
2014-08-09 22:33:18 +0800

    08/09

---
+++ network_file/time.c
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  time.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年08月06日 19时14分46秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sweetheart502, liyingxiao502@gmail.com
+ *        Company:  Class 1304 of Software Engineering
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+	char str[55];
+
+	time_t now;
+
+	time ( &now );
+
+	strcpy ( str, ctime ( &now ) );
+
+	printf ( "%s", str );
+
+	return EXIT_SUCCESS;
+}
+
