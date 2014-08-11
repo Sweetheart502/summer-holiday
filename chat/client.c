f6dd7e2f9d86267254332001a522337b1da24d9f
Sweetheart502 <869780307@qq.com>
2014-08-11 21:45:54 +0800

    08/11 -- 21:44

---
+++ chat/client.c
@@ -0,0 +1,14 @@
+#include "head.h"
+#include "menu.c"
+#include "my_err.c"
+#include "regist.c"
+#include "login.c"
+#include "link.c"
+#include "user_login.c"
+
+int main ( int argc, char **argv )
+{
+	menu ();
+
+	return 0;
+}
